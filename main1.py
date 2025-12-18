4ac1be27f5d304287eb1229d3b109ae24b2edd9e
Matt Sky L.Razonable <razonable.ml@obmontessori.edu.ph>
2025-12-18 12:41:18 +0800

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,4 +1,4 @@
-from pyscript import display, document
+from pyscript import document
 
 club_info = {
     "guitar": {
@@ -8,28 +8,11 @@ club_info = {
     }
 }
 
-def show_club_info(e):
+def show_club_info(e=None):
     info = club_info["guitar"]
-    
-    output = (
-        "<div style='display:flex; font-family:Playfair Display, serif;'>"
-            
-            "<div style='width:50%; padding-right:30px;'>"
-                "<div style='color:#FF5733; font-size:36px; font-weight:bold; margin-bottom:10px;'>Description</div>"
-                f"<div style='font-size:28px; color:#1C1C1C;'>{info['description']}</div>"
-            "</div>"
 
-            "<div style='width:50%; padding-left:30px;'>"
-                "<div style='color:#FF5733; font-size:32px; font-weight:bold; margin-bottom:5px;'>Meeting Time</div>"
-                f"<div style='font-size:26px; color:#1C1C1C;'>{info['meeting_time']}</div>"
-                
-                "<div style='color:#FF5733; font-size:32px; font-weight:bold; margin-top:15px; margin-bottom:5px;'>Location</div>"
-                f"<div style='font-size:26px; color:#1C1C1C;'>{info['location']}</div>"
-            "</div>"
-        "</div>"
-    )
-    
-    display(output, target="club-info-container")
+    document.querySelector(".club-description").innerHTML = "<div class='category-title'>Description</div>" + info["description"]
+    document.querySelector(".club-time").innerHTML = "<div class='category-title'>Meeting Time</div>" + info["meeting_time"]
+    document.querySelector(".club-location").innerHTML = "<div class='category-title'>Location</div>" + info["location"]
 
-js.populateClubInfo = populateClubInfo
 
